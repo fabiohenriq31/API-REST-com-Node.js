0b51a6ccd9bcb759e0e0dc1e00fc9ff48a4d0c3e
“fabiohenriq31” <121520452+fabiohenriq31@users.noreply.github.com>
2024-04-24 18:27:23 -0300

    Modification: Changed the listen

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,9 @@ import { app } from './app'
 
 app
   .listen({
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log('HTTP Server running!')
+    console.log('HTTP Server Running! 🚀')
   })
